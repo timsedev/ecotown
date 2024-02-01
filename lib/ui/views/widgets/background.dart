6f9dfc57946acf83526f7c9a2ba7ecea748095ec
timsedev <timsedev@gmail.com>
2024-02-01 14:31:28 +0800

    Created background widget for animated backgorund

---
+++ lib/ui/views/widgets/background.dart
@@ -0,0 +1,46 @@
+import 'package:animated_background/animated_background.dart';
+import 'package:flutter/material.dart';
+
+class Background extends StatefulWidget {
+  final Widget child;
+  const Background({super.key, required this.child});
+
+  @override
+  State<Background> createState() => _BackgroundState();
+}
+
+class _BackgroundState extends State<Background> with TickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height,
+      width: double.infinity,
+      child: AnimatedBackground(
+        behaviour: SpaceBehaviour(),
+        vsync: this,
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            margin:
+                const EdgeInsets.only(top: 30, left: 30, right: 30, bottom: 50),
+            height: 550,
+            width: 400,
+            // add shadow bellow
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 1,
+                  offset: const Offset(0, 5),
+                ),
+              ],
+            ),
+            child: widget.child,
+          ),
+        ),
+      ),
+    );
+  }
+}
