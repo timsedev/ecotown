af684e6636aab7513fbcd568a5dfccde0fa34c61
timsedev <timsedev@gmail.com>
2024-02-02 07:48:11 +0800

    Initialised menu dialog

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,6 +4,7 @@ import 'package:ecotown/ui/views/home/home_view.dart';
 import 'package:ecotown/ui/views/startup/startup_view.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
+import 'package:ecotown/ui/dialogs/menu/menu_dialog.dart';
 // @stacked-import
 
 @StackedApp(
@@ -24,7 +25,8 @@ import 'package:stacked_services/stacked_services.dart';
   ],
   dialogs: [
     StackedDialog(classType: InfoAlertDialog),
-    // @stacked-dialog
+    StackedDialog(classType: MenuDialog),
+// @stacked-dialog
   ],
 )
 class App {}
